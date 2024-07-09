3f1d8a39c10b6609e5eac1f80b0f88c684deaf43
Parth-2005 <pjpanot260305@gmail.com>
2024-07-09 22:26:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Contact from './sections/Contact/Contact';
+import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,21 @@
+import styles from './ProjectsStyles.module.css';
+import weather from '../../assets/weather.svg';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={weather}
+          link="https://github.com/Parth-2005/weather-app.git"
+          h3="Weather App"
+          p="Weather app built using Flutter and OpenWeatherMap API."
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
